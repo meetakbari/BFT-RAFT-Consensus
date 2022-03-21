5aff4f6ebb4500995ec86c95db7eeb96da59667e
meetakbari <meetakbari.me@gmail.com>
2022-03-21 20:19:05 +0530

    Create config and GetStorageLocation methods added in config.py

--- raft/server/config.py
+++ raft/server/config.py
@@ -27,3 +27,43 @@ class Config:
         self.private_key = PKCS1_PSS.new(private_key)
         self.address = address
         self.client_key = PKCS1_PSS.new(client_key)
+
+    """ Return a new config that is based off of the json given in parameter"""
+    def CreateConfig(cfg_filename, nodeId, debug=True):
+        with open(cfg_filename) as f:    
+            d = json.load(f)
+        assert "StorageDir" in d
+        assert "cluster" in d
+        assert "keyDir" in d
+        keyDir = d["keyDir"]
+        n = d["cluster"]
+        # get the public keys g
+        public_keys = utils.importPublicKeys(keyDir, len(n))
+        assert len(public_keys) == len(n)
+        # get the private key
+        if nodeId == -1:
+            private_key = utils.importClientPrivateKey(keyDir)
+        else:
+            private_key = utils.importPrivateKey(keyDir, nodeId)
+        client_key = utils.importClientPublicKey(keyDir)
+        clusterMap = {}
+        for i, l in enumerate(d["cluster"]):
+            assert len(l) == 2
+            clusterMap[(l[0], l[1])] = public_keys[i] # (addr, port) -> public key
+        address = tuple(d["cluster"][nodeId])
+        return Config(d["StorageDir"], clusterMap, nodeId, private_key, address, client_key, debug)
+    
+    """ Get the storage information for one node"""
+    def getStorageLocation(self,index):
+        assert index < len(self.cluster)
+        return "%s/%d.storage" % (self.storageDir, index) # storageDir/{index}.storage
+
+    def getMyStorage(self):
+        return self.getStorageLocation(self.nodeID)
+    
+    def getMyClusterInfo(self):
+        """ return the tuple (ip addr, port) for this node"""
+        return self.address
+
+    def getMyPrivateKey(self):
+        return self.private_key
