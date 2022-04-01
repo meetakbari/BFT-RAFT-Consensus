b27b5b02571e928c913fd4ccb3614c60b2f44206
meetakbari <meetakbari.me@gmail.com>
2022-04-01 16:33:17 +0530

    getLeaderForTerm and data received for client and peer methods added in states file

--- raft/server/states.py
+++ raft/server/states.py
@@ -3,6 +3,9 @@ import logging
 import statistics
 from random import randrange
 from os.path import join
+from raft.server.utils import PersistentDict, TallyCounter
+from raft.server.log import LogManager
+from raft.server.utils import get_kth_largest, get_quorum_size, validate_entries, signDict, validateDict, getLogHash, validateIndex
 import raft.server.config as cfg
 import Crypto
 logger = logging.getLogger(__name__)
@@ -35,3 +38,28 @@ class State:
             self._update_cluster()
         self.term_change_messages = {} # Map of term -> term change messages for that term (for terms for which you are a valid leader)
         self.stats = TallyCounter(['read', 'write', 'append'])
+
+    def getLeaderForTerm(self, term):
+        index = term % len(self.volatile['cluster'])
+        return list(self.volatile['cluster'])[index]
+
+    def data_received_peer(self, peer, msg):
+        """Receive peer messages from orchestrator and pass them to the
+        appropriate method."""
+        logger.debug('Received %s from %s', msg['type'], peer)
+
+        method = getattr(self, 'on_peer_' + msg['type'], None)
+        if method:
+            method(peer, msg)
+        else:
+            logger.info('Unrecognized message from %s: %s', peer, msg)
+
+    def data_received_client(self, protocol, msg):
+        """Receive client messages from orchestrator and pass them to the
+        appropriate method."""
+        method = getattr(self, 'on_client_' + msg['type'], None)
+        if method:
+            method(protocol, msg)
+        else:
+            logger.info('Unrecognized message from %s: %s',
+                        protocol.transport.get_extra_info('peername'), msg)
